4322c6184f005dd9f05687782f9a6f2e43f17a25
Engry.Mob <ralf.bolz@posteo.de>
2017-09-07 20:43:53 +0200

    udemy fe code

---
+++ app/proposal/proposal.ts
@@ -0,0 +1,16 @@
+export class Proposal {
+  constructor (
+    public id?: number,
+    public customer?: string,
+    public portfolio_url: string = 'http://',
+    public tools?: string,
+    public estimated_hours?: number,
+    public hourly_rate?: number,
+    public weeks_to_complete?: number,
+    public client_email?: string,
+
+  ){}
+}
+
+// 'Abc Company', 'http://portfolio.jordanhudgens.com', 'Runby on Rails',
+// 150,120, 15, 'jordan@devcamp.com'
